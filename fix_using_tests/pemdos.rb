1aa85a01071126c0b65062667ff73d4746f03326
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-01 18:19:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,9 +2,10 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10.times print "s"
-    end
-    + string
+    10.times do
+      print "s"
+      end
+      + string
   else
     string
   end
